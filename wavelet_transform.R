26165ad59f2ec0d13cd182757d897593088bbc11
keith <keithmllrnew@gmail.com>
2023-12-29 15:33:27 -0600

    using different wavelet

--- wavelet_transform.R
+++ wavelet_transform.R
@@ -2,25 +2,21 @@
 wavelet_transform <- function(x, len, iter, res){
   output <- matrix(nrow = iter, ncol = (length(x)+len))
   for(n in 1:iter){
-    wavelet <- SWG(len, n, res)
+    wavelet <- exp_wavelet(len, n, res, 0.2)
     output_vec <- convolve(wavelet, x, type = "open")
     output[n,] = output_vec
   }
   return(output)
 }
 
-SWG <- function(len, n){
+exp_wavelet <- function(len, n, res, alpha){
   
   mid = len%/%2
   nums = seq(from = -mid, to = mid)
-  t_neg = nums[c(1:mid)]/1000
-  t_pos = nums[c((mid+2):(len+1))]/1000
-  x_neg = complex(argument = -2*pi*t_neg)*sin((2^n)*pi*t_neg)/((2^n)*pi*t_neg)
-  x_pos = complex(argument = -2*pi*t_pos)*sin((2^n)*pi*t_pos)/((2^n)*pi*t_pos)
-  x_zero = 1 +0i
-  
-  x = append(x_neg, x_zero)
-  x = append(x, x_pos)
-  x = x *(2^(n/2))
+  t = nums/len
+  theta = pi*res*n
+  x = complex(argument = -2*theta*t)*exp(-alpha*theta*abs(t))
+  x = x*(theta*alpha)
+  return(x)
   
 }
